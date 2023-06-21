c41e0b3ccfef58b8fb4351dd562f0b46c792fc5d
Vladislav1809 <lik_200@mail.ru>
2023-06-21 14:49:51 +0300

    блокировка комбиков

--- Form3.cs
+++ Form3.cs
@@ -15,6 +15,21 @@ namespace Экз_ТП_WF
         public Form3()
         {
             InitializeComponent();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox7.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox8.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox9.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox10.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox11.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox12.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox13.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox14.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox15.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
         }
 
         private void label16_Click(object sender, EventArgs e)
